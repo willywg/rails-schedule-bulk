3f42357f9422318679701ebc60c3cead0e999994
WillyWG <willywg@gmail.com>
2020-05-09 16:15:11 -0500

    Add Shoulda Matchers

---
+++ spec/support/shoulda_matchers.rb
@@ -0,0 +1,9 @@
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+  end
+end
