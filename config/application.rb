5ed6ac731b29fe483b544c1f6dda38f8f0390888
WillyWG <willywg@gmail.com>
2020-05-09 16:18:44 -0500

    Setup rails generators

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,17 @@ module SchedulesTest
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false 
+    end
   end
 end
