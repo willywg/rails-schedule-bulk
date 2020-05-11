bfbf140427e41e92a9a65b1d78b3db29be7a1875
WillyWG <willywg@gmail.com>
2020-05-10 19:54:55 -0500

    BulkSchedulecontroller creates schemas in bulk

--- app/controllers/bulk_schedules_controller.rb
+++ app/controllers/bulk_schedules_controller.rb
@@ -4,6 +4,22 @@ class BulkSchedulesController < ApplicationController
   end
 
   def create
-    render plain: params.inspect
+    @bulk_schedule = Forms::BulkSchedule.new(bulk_schedule_params)
+
+    if @bulk_schedule.save
+      redirect_to root_path, notice: 'Horarios creados con éxito!!!'
+    else
+      render :new
+    end
+    # render plain: params.inspect
   end
+
+  private
+    def bulk_schedule_params
+      params.require(:bulk_schedule).permit(
+        :start_at,
+        :end_at,
+        :days => {}
+      )
+    end
 end
